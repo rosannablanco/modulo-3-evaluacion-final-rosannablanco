6f36c5416e7e997aba04bc3cbc4f488c82471771
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-04-23 20:11:02 +0200

    update folder docs

---
+++ docs/precache-manifest.52d210f7824fbba86b568eb51500f871.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9133da34f5b574f3e174eeb377cda942",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4fe1fdf677e2ee93d28d",
+    "url": "./static/css/main.dcd1036b.chunk.css"
+  },
+  {
+    "revision": "ae1a4203fbbf6089b481",
+    "url": "./static/js/2.684cfdcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.684cfdcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe1fdf677e2ee93d28d",
+    "url": "./static/js/main.d28bf29f.chunk.js"
+  },
+  {
+    "revision": "bc3a119b8b65ffea2057",
+    "url": "./static/js/runtime-main.bbea56ad.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick_and_Morty_-_logo_(English).75509f2e.png"
+  },
+  {
+    "revision": "422193e57361fb677340788992e64a27",
+    "url": "./static/media/alien.422193e5.svg"
+  },
+  {
+    "revision": "2ef5ea9a243af5bfb9df3037eda348cd",
+    "url": "./static/media/died.2ef5ea9a.svg"
+  },
+  {
+    "revision": "7c2c3ee26b25cea8a26c3575a5bb6cfb",
+    "url": "./static/media/human.7c2c3ee2.svg"
+  }
+]);
