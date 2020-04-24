b8da7a05b1dd688d568d8e8a2406b764d726c8bc
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-04-24 12:32:41 +0200

    Add attribute alt and title in image of CharacterCard component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,7 +5,7 @@ function CharacterCard(props) {
   return (
     <li className="CharacterLi">
       <Link to={`/${props.character.id}`} className="link">
-        <img className="Img" src={props.character.image} title="" alt="" />
+        <img className="Img" src={props.character.image} title={`foto de ${props.character.name}`} alt={`foto de ${props.character.name}`} />
         <div className="ContainerInfo">
           <h4>{props.character.name}</h4>
           <p>{props.character.species}</p>
